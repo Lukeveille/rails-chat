b875031e7d281f35866da014e9e47c192c9b1bb9
Lukeveille <luke@lukeleveille.com>
2019-05-21 08:32:29 -0400

    Action Cable is working

--- app/channels/room_channel.rb
+++ app/channels/room_channel.rb
@@ -1,5 +1,6 @@
 class RoomChannel < ApplicationCable::Channel
   def subscribed
-    stream_from "room_#{params[:room]}"
+    room = Room.find params[:room]
+    stream_for room
   end
 end
--- app/controllers/room_messages_controller.rb
+++ app/controllers/room_messages_controller.rb
@@ -2,7 +2,7 @@ class RoomMessagesController < ApplicationController
   before_action :load_entities
 
   def create
-    @room_messages = RoomMessage.create user: current_user, room: @room, message: params.dig(:room_message, :message)
+    @room_message = RoomMessage.create user: current_user, room: @room, message: params.dig(:room_message, :message)
 
     RoomChannel.broadcast_to @room, @room_message
   end
